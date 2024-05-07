919b8ffb04846497b305c382500ced90b89548cf
Andres Morales <moralesandres@outlook.com>
2024-05-07 14:31:48 -0500

    5.2

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,14 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
-  // console.log(blogs)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -22,6 +29,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       setUser(user)
       setUsername('')
       setPassword('')
@@ -55,11 +65,17 @@ const App = () => {
       <button type="submit">login</button>
     </form>      
   )
-  
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
   const blogForm = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged in </p>
+      <button onClick={handleLogout}>logout</button>
+      
       {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
       )}
